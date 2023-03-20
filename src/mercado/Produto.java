40523bbcdc84ce460a2aa633b2500d5118041e43
gabrieldnlc <83305378+gabrieldnlc@users.noreply.github.com>
2023-03-20 09:48:58 -0300

    primeiro commit
    exemplos de código

---
+++ src/mercado/Produto.java
@@ -0,0 +1,21 @@
+package mercado;
+
+public class Produto
+{
+	private String nome;
+	private String fabricante;
+	private double preco;
+	
+	public String getNome() { return nome; }
+	public String getFabricante() { return fabricante; }
+	public double getPreco() { return preco; }
+	
+	public Produto(String nome, String fabricante, double preco)
+	{
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.preco = preco;
+	}
+
+	
+}
---
+++ src/mercado/Repositorio.java
@@ -0,0 +1,8 @@
+package mercado;
+
+import java.util.Map;
+
+public class Repositorio
+{
+
+}
